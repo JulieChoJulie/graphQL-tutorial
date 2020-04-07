9941a2ef1121344273d5a821b8ace1f584a4a48d
JulieChoJulie <h32cho@gmail.com>
2020-04-07 19:18:40 -0400

    first commit for react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div className="main">
+        <h1>Hello</h1>
+        <BookList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BookList = () => {
+    return (
+        <div>
+            <ul id="book-list">
+                <li>Hello</li>
+                <li>Hello2</li>
+                <li>Hello3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default BookList;
